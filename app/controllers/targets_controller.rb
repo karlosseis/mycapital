3c3d7ae49f99296196a87d0524cae5009bf4d1f4
root <root@ubuntu.defaultdomain>
2017-05-04 22:04:01 +0000

    solución stock prices mayores de 1000

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -4,6 +4,7 @@ class TargetsController < ApplicationController
   	 @greens = current_user.companies.verde
   	 @yellows = current_user.companies.amarillo
   	 @greys = current_user.companies.gris
+  	 @reds = current_user.companies.rojo
 
   end
 end
