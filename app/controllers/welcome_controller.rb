e5173ae09f64d26957d07a589663ad15eb6b7b86
root <root@ubuntu.defaultdomain>
2017-08-29 19:19:46 +0000

    brokers

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,11 +12,7 @@ class WelcomeController < ApplicationController
      @next_dates_to_buy  = {}  # fechas límite por boker para comprar
 
      current_user.companies.all.each do |company| 
-      unless company.porc_dif_target_price.nil? or company.target_price_1.nil?
-	      if company.porc_dif_target_price.to_f <= 1 and company.target_price_1 > 0
-	        @stars[company.id] = company      
-	      end      	
-      end
+
   	  unless company.porc_dif_target_sell_price.nil? or company.target_sell_price.nil?	
 	      if company.porc_dif_target_sell_price.to_f <= 5 and company.target_sell_price > 0
 	        @to_sell[company.id] = company      
@@ -32,11 +28,16 @@ class WelcomeController < ApplicationController
         @max_bajadas[company.id] = company      
       end
 
-
-      if company.next_dividend_date.nil? or company.next_dividend_date < Date.today
-        @por_revisar_div[company.id] = company      
+      unless company.rojo? # si es roja no quiero que me salga para actualziar dividendos ni para comprar
+        if company.next_dividend_date.nil? or company.next_dividend_date < Date.today
+          @por_revisar_div[company.id] = company      
+        end
+        unless company.porc_dif_target_price.nil? or company.target_price_1.nil?
+          if company.porc_dif_target_price.to_f <= 1 and company.target_price_1 > 0
+            @stars[company.id] = company      
+          end       
+        end        
       end
-
      end
 
      current_user.brokers.all.each do |broker|
