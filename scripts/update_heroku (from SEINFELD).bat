3ad498d1cfe0cf6b5446a522167c67cbf7909f0d
root <root@ubuntu.defaultdomain>
2017-09-09 15:24:44 +0000

    recuperacion despues de desaparecer carpeta git

--- scripts/update_heroku (from SEINFELD).bat
+++
@@ -1,3 +0,0 @@
-heroku run --app limitless-gorge-32493 rails runner "SharePriceUpdateFromgooglefinanceJob.perform_now"
-heroku run --app limitless-gorge-32493 rails runner "PopulateExpectedDividendsJob.perform_now"
-heroku run --app limitless-gorge-32493 rails runner "PopulateEstimatedMovementsJob.perform_now"
