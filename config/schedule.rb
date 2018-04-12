6b86ec4cace4f8e192b7ba39a566ddfc7e56ef37
root <root@ubuntu.defaultdomain>
2018-04-12 15:53:53 +0000

    calculo next dividend y tasa de cambio

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,8 +13,35 @@
 #   rake "some:great:rake:task"
 # end
 #
+
+
+##  PARA AÑADIRLO AL CRONTAB ###
+## OJO, pone bin/rails y hay que dejar rails a secas.
+# Change to your application's directory.
+# cd /my/app
+
+# View config/schedule.rb converted to cron syntax
+# bundle exec whenever
+
+# Update crontab
+# bundle exec whenever -i
+
+# Overwrite the whole crontab (be careful with this one!)
+# bundle exec whenever -w
+
+# See all the options for the whenever command
+# bundle exec whenever -h
+
+
  every 5.minutes do
    runner "SharePricesUpdateJob.perform_now"
  end
+ every 7.minutes do
+   runner "UpdateCompaniesHeaderSumFields.perform_now"
+ end
 
+every 1.day, :at => '8:28 am' do
+  runner "PopulateExpectedDividendsJob.perform_now"
+  runner "PopulateEstimatedMovementsJob.perform_now"
+end
 # Learn more: http://github.com/javan/whenever
