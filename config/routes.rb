2b211367c12db91c15c13f72944fce575165dcea
root <root@ubuntu.defaultdomain>
2020-03-16 07:11:13 +0000

    cambio version

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :company_historic_dividends
   get 'targets/index'
   get 'welcome/sindiv'
+  get 'welcome/exchange_rates'
 
 
 resources :excel_import do
