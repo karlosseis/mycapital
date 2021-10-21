1d19fd573f21794fcdbfe2cfc6935ebb160c0691
root <root@ubuntu.defaultdomain>
2021-10-21 05:43:02 +0000

    tabs

--- app/models/iex.rb
+++ app/models/iex.rb
@@ -16,10 +16,12 @@ class Iex
 
     
     #angulo.carlos
-    @secret_token = "sk_a869f8d2eba140728de5380d3b971899"
+    #@secret_token = "sk_a869f8d2eba140728de5380d3b971899"
 
+
+    # 21/10/2021
     #corotos
-    #@secret_token = "sk_0af9b76d64e744eb99d90e8f8a525da6"
+    @secret_token = "sk_0af9b76d64e744eb99d90e8f8a525da6"
     
   end
 
